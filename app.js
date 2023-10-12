00a5532e748726467096601ec13dbd7ff4dc3c72
kongukjae <kongukjae@gmail.com>
2023-10-12 10:57:07 +0900

    향마루 응답처리

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const http = reqire('http');
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  res.writeHead (200, {"Content-Type": "text/plain" });
+  res.write('향마루');
+  res.end();
 });
 
-server.listen(3500, (err) => {
-  if(!err) {
-    console.error ('서버 안돌아감');
-  } else {
-    console.log("3500 포트에 돌아감 http://localhost:3500");
-  }
-})
+server.listen(3500);
