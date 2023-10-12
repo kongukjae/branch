72e35d418e00b306a1c1959676b613be04f119c1
kongukjae <kongukjae@gmail.com>
2023-10-12 10:42:54 +0900

    기본 서버 제작

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const http = reqire('http');
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(3500, (err) => {
+  if(!err) {
+    console.error ('서버 안돌아감');
+  } else {
+    console.log("3500 포트에 돌아감 http://localhost:3500");
+  }
+})
